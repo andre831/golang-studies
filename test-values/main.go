f12a7b79504d9f7e389245fc4c6bfade78c75e3f
Andre Andre Andre <andredupdm@gmail.com>
2023-03-01 23:14:47 -0300

    add values types

---
+++ test-values/main.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+  fmt.Println("Ba" + "nana")
+
+  fmt.Println("2+2 =", 2+2)
+  fmt.Println("7.0/3.0 =", 7.0/3.0)
+
+  fmt.Println(true && false)  // false
+  fmt.Println(true || false) // true
+  fmt.Println(!true)  //false
+}
